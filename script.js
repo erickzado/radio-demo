ecf4e9894a3347b173d6294adcd69e2b390f21a0
erickzado <erickzado.bolivar@gmail.com>
2025-07-06 10:19:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-fetch('stations.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('stations');
-    data.forEach(station => {
-      const div = document.createElement('div');
-      div.innerHTML = `
-        <h2>${station.name}</h2>
-        <p>${station.city}, ${station.country}</p>
-        <p>${station.genre}</p>
-        <audio controls src="${station.stream}"></audio>
-        <hr/>
-      `;
-      container.appendChild(div);
-    });
-  });
+
